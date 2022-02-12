f4573b6307d672e8801cc8c6d35a8f6d840dad36
sviatvguss <sviat.v.guss@gmail.com>
2022-02-12 17:41:59 +0600

    My solution

---
+++ main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	. "solution-hello"
+)
+
+func main() {
+	s := GetMessage()
+	fmt.Println(s)
+	fmt.Println("All right!")
+}
--- solution-hello.go
+++ solution-hello.go
@@ -1,6 +1,8 @@
 package solution
 
-import "github.com/kyokomi/emoji"
+import (
+	"github.com/kyokomi/emoji"
+)
 
 func GetMessage() string {
 	return emoji.Sprint("Hello :world_map:!")
